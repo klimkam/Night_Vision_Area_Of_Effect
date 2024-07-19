17d0154a9cb1cd561344e95801c6ed3686749d5a
Klimentiy Mandra <56902939+klimkam@users.noreply.github.com>
2024-07-19 15:20:24 -0400

    Outlines with green light

--- Assets/Script/NightVision.cs
+++ Assets/Script/NightVision.cs
@@ -5,6 +5,8 @@ public class NightVision : MonoBehaviour
 {
     [SerializeField] private Color m_defaultLightColour;
     [SerializeField] private Color m_boostedLightColour;
+    [SerializeField] private Material m_outlineMaterial;
+    [SerializeField] private int m_rendererID = Shader.PropertyToID("_OffsetMultiplier");
 
     private bool m_isNightVisionEnabled;
     private Volume m_volume;
@@ -28,16 +30,19 @@ public class NightVision : MonoBehaviour
     private void ToggleNightVision()
     {
         m_isNightVisionEnabled = !m_isNightVisionEnabled;
-
+        m_rendererID = 0;
         if (m_isNightVisionEnabled)
         {
             RenderSettings.ambientLight = m_boostedLightColour;
             m_volume.weight = 1;
+            
+            m_outlineMaterial.SetFloat("OffsetMultiplier", 1);
         }
         else
         {
             RenderSettings.ambientLight = m_defaultLightColour;
             m_volume.weight = 0;
+            m_outlineMaterial.SetFloat("OffsetMultiplier", 0);
         }
     }
 }
